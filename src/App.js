3f1562b798828092f2a684b413acbd3b851e2acf
ala decoste <alshareefialaa@gmail.com>
2021-02-24 02:50:15 -0500

    Created React project.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './App.css';
+import EmployeeDir from './components/EmployeeDir/index.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+      <EmployeeDir/>
+  );
+}
+
+export default App;
