833d4c3adc1b01a901c5e23bac6e11895ac1f242
ala decoste <alshareefialaa@gmail.com>
2021-02-24 02:52:13 -0500

    Added Nav component.

---
+++ src/components/Nav/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+import Navbar  from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+
+function Nav() {
+ return <>
+<Navbar expand="lg" className="color">
+  <Container >
+    <Navbar.Brand href="#" className="center-navbar">
+      <b>Employee Directory</b>
+      </Navbar.Brand>
+  </Container>
+</Navbar>
+ </>
+}
+
+export default Nav;
