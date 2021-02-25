ded271e7224afc2995c3a22173343b598186ab3d
ala decoste <alshareefialaa@gmail.com>
2021-02-25 04:11:36 -0500

    test Heroku

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const router = require("express").Router();
 
 router.use(function (req, res) {
-  res.sendFile(path.join(__dirname,  "../client/index.html"));
+  res.sendFile(path.join(__dirname,  "../client/build/index.html"));
 });
 
 module.exports = router;
